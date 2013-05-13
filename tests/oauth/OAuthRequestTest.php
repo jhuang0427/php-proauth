9880ba6db124d0d786181aff30262816637c7633
Rob Loach <robloach@gmail.com>
2013-05-13 09:40:52 -0400

    Add php-proauth

---
+++ tests/oauth/OAuthRequestTest.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once '../../lib/oauth/OAuthClient.php';
+require_once 'PHPUnit/Framework.php';
+
+class OAuthRequestTest extends PHPUnit_Framework_TestCase
+{
+	/*public function test()
+	{
+	}*/
+}
